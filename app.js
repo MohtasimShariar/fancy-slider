c7df3114e5010c33a2b6ab19779f54ef5e20d4b2
Md.Mohtasim Shariar <onim04@gmail.com>
2021-02-19 13:10:48 +0600

    final

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+const searchInput = document.getElementById('search')
+
+//search to hit Enter key
+
+searchInput.addEventListener("keypress", function(event) {
+    if (event.key == 'Enter'){
+      searchBtn.click();
+    }    
+});
+
 // selected image 
 let sliders = [];
 
@@ -15,6 +25,10 @@ const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 // show images 
 const showImages = (images) => {
+  if(images.length === 0 || searchInput.value === ""){
+    alert("The search box cannot be empty or item does not exist");
+    imagesArea.style.display = 'none';
+  }else{
   imagesArea.style.display = 'block';
   gallery.innerHTML = '';
   // show gallery title
@@ -24,11 +38,13 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner(false);
   })
-
+}
 }
 
 const getImages = (query) => {
+  toggleSpinner(true);
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
@@ -38,13 +54,13 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    sliders.splice(item, 1);
   }
 }
 var timer
@@ -67,8 +83,14 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
-  slider.forEach(slide => {
+  let duration = document.getElementById('duration').value || 1000;
+  if(duration >= 1000){
+    duration = duration;
+  }else{
+    alert('Duration cannot be negative or less then 1 second');
+    duration = 1000;
+  }
+  sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
     item.innerHTML = `<img class="w-100"
@@ -113,10 +135,21 @@ searchBtn.addEventListener('click', function () {
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
   const search = document.getElementById('search');
-  getImages(search.value)
+  getImages(search.value);
   sliders.length = 0;
-})
+});
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
-})
+});
+
+//spinner
+
+const toggleSpinner =(show) =>{
+  const spinner = document.getElementById('loading-spinner');
+  if(show){
+    spinner.classList.remove('d-none');
+  }else{
+    spinner.classList.add('d-none');
+}
+};
